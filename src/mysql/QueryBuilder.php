672213f49a30cc231dd9e0bd692dfc9e0b6f2f58
amoydavid <liuw@liuw.net>
2018-01-20 14:14:23 +0800

    use Yii QueryBuilder to build SQL

--- src/mysql/QueryBuilder.php
+++ src/mysql/QueryBuilder.php
@@ -3,65 +3,24 @@
 namespace amoydavid\Yii2LockableQuery\mysql;
 
 use amoydavid\Yii2LockableQuery\Query;
-use yii\db\Expression;
 
 
 /**
  * QueryBuilder is the query builder for MySQL databases.
  *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
+ * @author amoydavid <liuw@liuw.net>
  */
 class QueryBuilder extends \yii\db\mysql\QueryBuilder
 {
 
     /**
-     * Generates a SELECT SQL statement from a [[Query]] object.
-     * @param Query $query the [[Query]] object from which the SQL statement will be generated.
-     * @param array $params the parameters to be bound to the generated SQL statement. These parameters will
-     * be included in the result with the additional parameters generated during the query building process.
-     * @return array the generated SQL statement (the first array element) and the corresponding
-     * parameters to be bound to the SQL statement (the second array element). The parameters returned
-     * include those provided in `$params`.
-     * @throws \yii\db\Exception
+     * @param \yii\db\Query $query
+     * @param array $params
+     * @return array
      */
     public function build($query, $params = [])
     {
-        $query = $query->prepare($this);
-
-        $params = empty($params) ? $query->params : array_merge($params, $query->params);
-
-        $clauses = [
-            $this->buildSelect($query->select, $params, $query->distinct, $query->selectOption),
-            $this->buildFrom($query->from, $params),
-            $this->buildJoin($query->join, $params),
-            $this->buildWhere($query->where, $params),
-            $this->buildGroupBy($query->groupBy),
-            $this->buildHaving($query->having, $params),
-        ];
-
-        $sql = implode($this->separator, array_filter($clauses));
-        $sql = $this->buildOrderByAndLimit($sql, $query->orderBy, $query->limit, $query->offset);
-
-        if (!empty($query->orderBy)) {
-            foreach ($query->orderBy as $expression) {
-                if ($expression instanceof Expression) {
-                    $params = array_merge($params, $expression->params);
-                }
-            }
-        }
-        if (!empty($query->groupBy)) {
-            foreach ($query->groupBy as $expression) {
-                if ($expression instanceof Expression) {
-                    $params = array_merge($params, $expression->params);
-                }
-            }
-        }
-
-        $union = $this->buildUnion($query->union, $params);
-        if ($union !== '') {
-            $sql = "($sql){$this->separator}$union";
-        }
+        list($sql , $params) = parent::build($query, $params);
 
         if($query instanceof Query && $query->forUpdate) {
             $sql .= ' FOR UPDATE';
